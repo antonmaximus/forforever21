c55a4c19dfe0f388ba062bf844febaf4d3cf8a06
aj <anton23@gmail.com>
2014-10-16 11:57:18 -0700

    forever and ever

--- config.rb
+++ config.rb
@@ -62,7 +62,7 @@ ignore 'stylesheets/Top-Secret-fontfacekit/misc/*'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
