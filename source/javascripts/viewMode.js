ce9ef6ca5336af95453458f71c7fdee7bf4dca65
aj <anton23@gmail.com>
2014-10-16 11:50:27 -0700

    forever and ever

---
+++ source/javascripts/viewMode.js
@@ -0,0 +1,21 @@
+/******************************************************************************
+*** VIEWMODE.JS 
+*** Author:    Anton Agana
+***
+*** Handles buttons presses to change viewing mode.
+***
+******************************************************************************/
+
+;(function($){
+  $('#viewThree').on('click', function() {
+    $('#imageHolder').removeClass( "columns4" ).addClass( "columns3" );
+    // Disable button 3 and Enable button 4
+    $(this).prop('disabled', true).next().prop('disabled', false);
+  });
+
+  $('#viewFour').on('click', function() {
+    $('#imageHolder').removeClass( "columns3" ).addClass( "columns4" );
+    // Disable button 4 and Enable button 3
+    $(this).prop('disabled', true).prev().prop('disabled', false);;
+  });
+})(jQuery);
